fb6f87f7f8f4386d17ef96744ba557e533ce7be9
s793618943 <793618943@qq.com>
2016-07-13 15:20:22 +0800

    场景移动

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,10 +2,58 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
+    QMainWindow(parent),dq(0),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    tu[0]=new QPixmap(":/A1");
+   tu[1]=new QPixmap(":/A2");
+   tu[2]=new QPixmap(":/A3");
+   tu[3]=new QPixmap(":/A4");
+   tuq[0]=new QRect(0,0,800,250);
+   tuq[1]=new QRect(800,0,800,250);
+   tuq[2]=new QRect(0,0,800,250);
+   tuq[3]=new QRect(0,0,800,250);
+
+}
+void MainWindow::paintEvent (QPaintEvent *event){
+    QPainter painter(this);
+    painter.drawPixmap(*tuq[dq],*tu[dq]);
+    painter.drawPixmap(*tuq[dq+1],*tu[dq+1]);
+}
+void MainWindow::keyPressEvent( QKeyEvent *event){
+    char ch;
+    ch=event->key();
+    int width1,height1;
+    int width = tuq[dq]->width ();
+    int height=tuq[dq]->height();
+    if(dq<4){ width1 = tuq[dq+1]->width ();
+     height1=tuq[dq+1]->height();}
+    switch(ch){
+    case 'a':
+    case 'A':tuq[dq]->setX(tuq[dq]->x()+4);tuq[dq+1]->setX(tuq[dq+1]->x()+4);break;
+    case 'd':
+    case 'D':tuq[dq]->setX(tuq[dq]->x()-4);tuq[dq+1]->setX(tuq[dq+1]->x()-4);break;
+    }
+
+    if(event->key()=='A'||event->key()=='a'){
+        if(tuq[dq]->x()>=0&&dq>0){
+            dq--;
+            tuq[dq]->setX(-796);
+            //~
+        }
+    }
+    if(event->key()=='D'||event->key()=='d'){
+        if(tuq[dq]->x()+800<=0&&dq<4){
+            dq++;
+            tuq[dq+1]->setX(796);
+        }
+    }
+    tuq[dq]->setWidth (width);
+     tuq[dq]->setHeight(height);
+    if(dq<4){ tuq[dq+1]->setWidth (width1);
+      tuq[dq+1]->setHeight(height1);}
+    update();
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,6 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include <QtGui>
 #include <QMainWindow>
 
 namespace Ui {
@@ -13,10 +13,17 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
+    void yidong();
+    void genghuan();
     ~MainWindow();
-
+protected:
+    void keyPressEvent( QKeyEvent *event);
+    void paintEvent (QPaintEvent *event);
 private:
     Ui::MainWindow *ui;
+    QPixmap *tu[4];
+    QRect *tuq[4];
+    int dq;
 };
 
 #endif // MAINWINDOW_H
