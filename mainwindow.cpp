d1cf5ece832d44feb94990061777308662b039e2
~（时之旅人）~ <~~>
2016-07-14 15:21:59 +0800

    window 重构

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,4 @@
 #include "mainwindow.h"
-#include<iostream>
-#include <QDebug>
 
 #include "ui_mainwindow.h"
 using namespace std;
@@ -8,32 +6,62 @@ using namespace std;
 #define FUN_JUMP(x) (1.8*(x)*(x)-36*(x)+175)
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),dq(0),tfang(0),jip(false),tdf(false),rfang(1),tu_peo_left(":/r_l"),tu_peo_right(":/r_r"),
-    tuq_peo(300,175,67,67),peo_jump(false),rtd(0),peo_move_pan(false),ffang(1),ji(0),
+    tuq_peo(300,175,67,67),peo_jump(false),rtd(0),ffang(1),ji(0),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     tu[0]=new QPixmap(":/A1");
-   tu[1]=new QPixmap(":/A2");
-   tu[2]=new QPixmap(":/A3");
-   tu[3]=new QPixmap(":/A4");
-   tu[4]=new QPixmap(":/A4");
-   tu_peo=&tu_peo_right;
-   tuq[0]=new QRect(0,0,800,250);
-   tuq[1]=new QRect(800,0,800,250);
-   tuq[2]=new QRect(0,0,800,250);
-   tuq[3]=new QRect(0,0,800,250);
-   tuq[4]=new QRect(0,0,800,250);
+    tu[1]=new QPixmap(":/A2");
+    tu[2]=new QPixmap(":/A3");
+    tu[3]=new QPixmap(":/A4");
+    tu[4]=new QPixmap(":/A4");
+    tu_peo=&tu_peo_right;
+    tuq[0]=new QRect(0,0,800,250);
+    tuq[1]=new QRect(800,0,800,250);
+    tuq[2]=new QRect(0,0,800,250);
+    tuq[3]=new QRect(0,0,800,250);
+    tuq[4]=new QRect(0,0,800,250);
     jishi1 =new QTimer(this);
     jishi2 =new QTimer(this);
-     connect( jishi1,SIGNAL(timeout()),this,SLOT(yidong()));
-      connect( jishi2,SIGNAL(timeout()),this,SLOT(yidong()));
-     connect(&peo_jump_timer,SIGNAL(timeout()),this,SLOT(peo_jump_set()));
-      connect(&peo_move_timer1,SIGNAL(timeout()),this,SLOT(peo_move()));
-      connect(&peo_move_timer2,SIGNAL(timeout()),this,SLOT(ting()));
+    connect( jishi1,SIGNAL(timeout()),this,SLOT(yidong()));
+    connect( jishi2,SIGNAL(timeout()),this,SLOT(yidong()));
+    connect(&peo_jump_timer,SIGNAL(timeout()),this,SLOT(peo_jump_set()));
+    connect(&peo_move_timer,SIGNAL(timeout()),this,SLOT(peo_move()));
+    connect(&key_board_timer,SIGNAL(timeout()),this,SLOT(key_bourd_fun()));
     hero.x=300;
     hero.y=175;
-
+    key_board_timer.start(30);
 }
+void MainWindow::key_bourd_fun(){
+    bool key[256]={false},flag=true;
+    key['A']=GetKeyState('A')<0?true:false;
+    key['S']=GetKeyState('S')<0?true:false;
+    key['D']=GetKeyState('D')<0?true:false;
+    key['W']=GetKeyState('A')<0?true:false;
+    key[VK_SPACE]=GetKeyState(VK_SPACE);
+    if(key['A']){
+        rfang=-1;
+    }else if(key['D']){
+        rfang=1;
+    }else if(key['S']){
+        rfang=-2;
+    }else if(key['w']){
+        rfang=2;
+    }else{
+        flag=false;
+    }
+    if(flag){
+        if(!peo_move_timer.isActive()){
+            peo_move_timer.start(TI);
+            //qDebug("hear\n");
+        }
+    }else{
+        if(peo_move_timer.isActive()){
+            peo_move_timer.stop();
+        }
+    }
+}
+
 void MainWindow::paintEvent (QPaintEvent *event){
     QPainter painter(this);
     painter.drawPixmap(*tuq[dq],*tu[dq]);
@@ -42,82 +70,20 @@ void MainWindow::paintEvent (QPaintEvent *event){
 
 }
 void MainWindow::keyPressEvent( QKeyEvent *event){
+
     char ch;
-
-
     ch=event->key();
-    qDebug("%d",rfang);
-    if(rtd==1){
-      switch(ch){
-      case 'a':
-      case 'A':
-          if(jip==false){
-              rfang=tfang=-1;
-              jishi1->start(TI);
+    //qDebug("%d",rfang);
 
-              jip=true;
-          }break;
-      case 'W':
-      case 'w':
-          if(peo_jump==false){
-              peo_jump=true;
-              peo_jump_timer.start(0.5*TI);
-          }
-          break;
-      case 'd':
-      case 'D':
-          if(jip==false){
-              rfang=tfang=1;
-              jishi2->start(TI);
-              jip=true;
-          }break;
-
-      }
-    }
-    if(rtd==0){
         switch(ch){
-        case 'a':
-        case 'A':
-
-           peo_move_timer2.start(485);
-            rfang=-1;
-            if(peo_move_pan==false){
-
-               peo_move_timer1.start(TI);
-
-                peo_move_pan=true;
-            }break;
-        case 'W':
+        case ' ':
         case 'w':
-
-              peo_move_timer2.start(4000);
             if(peo_jump==false){
                 peo_jump=true;
                 peo_jump_timer.start(3*TI);
             }
             break;
-        case 'd':
-        case 'D':
-
-               peo_move_timer2.start(485);
-              rfang=1;
-            if(peo_move_pan==false){
-
-               peo_move_timer1.start(TI);
-
-                peo_move_pan=true;
-
-            }break;
-
-        }
     }
-    }
-
-void MainWindow::ting(){
-    qDebug("%d",rfang);
-    peo_move_timer1.stop();
-     peo_move_timer2.stop();
-    peo_move_pan=false;
 }
  /*void MainWindow::keyReleaseEvent( QKeyEvent *event){
    if(event->isAutoRepeat()==false){
@@ -207,8 +173,16 @@ void MainWindow::peo_move(){
     }*/
 
     switch(rfang){
-    case 1:hero.x+=2;tuq_peo.setRect(hero.x,hero.y,67,67);tu_peo=&tu_peo_right;break;
-    case -1:hero.x-=2;tuq_peo.setRect(hero.x,hero.y,67,67);tu_peo=&tu_peo_left;break;
+    case 1:
+        hero.x+=2;
+        tuq_peo.setRect(hero.x,hero.y,67,67);
+        tu_peo=&tu_peo_right;
+        break;
+    case -1:
+        hero.x-=2;
+        tuq_peo.setRect(hero.x,hero.y,67,67);
+        tu_peo=&tu_peo_left;
+        break;
 }
 
      repaint();
--- mainwindow.h
+++ mainwindow.h
@@ -34,20 +34,21 @@ private:
     QPixmap *tu_peo;
     QRect *tuq[5];
     QRect tuq_peo;
- struct ren{
-    int x,y;
-    int h,w;
- };
- ren hero;
+    struct ren{
+        int x,y;
+        int h,w;
+    };
+    ren hero;
     int dq;
-    QTimer *jishi1,*jishi2,peo_jump_timer,peo_move_timer1,peo_move_timer2;
-    bool jip,tdf,rdf,peo_jump,peo_move_pan;
+    QTimer *jishi1,*jishi2,peo_jump_timer,peo_move_timer,key_board_timer;
+    bool jip,tdf,rdf,peo_jump;
     int tfang,rfang,rtd,ffang,ji;
     public slots:
- void yidong();
-  void peo_jump_set();
-  void peo_move();
-  void ting();
+    void yidong();
+    void peo_jump_set();
+    void peo_move();
+    //void ting();
+    void key_bourd_fun();
 };
 
 #endif // MAINWINDOW_H
