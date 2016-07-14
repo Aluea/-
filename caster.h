a733d516b402dca3fabd2955f7b0c9ca885799ad
~（时之旅人）~ <~~>
2016-07-14 20:50:58 +0800

    no message

--- caster.h
+++ caster.h
@@ -6,15 +6,28 @@
 #include<life.h>
 #include<m_map.h>
 #define EMPTY -1
-typedef struct{
+struct pic{
     bool show;
     int id,x,y,width,height;
-}pic;
-typedef struct{
+};
+struct pic_new{
     char name;//m为背景 p为人物
+<<<<<<< HEAD
     int x,y,width,height;
 }pic_new;
+=======
+    int id,x,y,width,height;
+};
+>>>>>>> 12c1250019679e5dc3b63498e96f9b28611ae9ba
 enum FANG{
     SHANG=0,XIA,ZUO,YOU
 };
+struct showturn{
+    int z;
+    int id;
+};
+bool cmp(showturn a,showturn b){
+    return a.z>b.z;
+}
+
 #endif
